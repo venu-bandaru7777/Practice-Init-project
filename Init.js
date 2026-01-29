120bdac120df56a7b836a9e4142131380dbb9b07
venu_007 <vbandaru@netscoretech.com>
2026-01-29 18:45:06 +0530

    Added 3 files in this project

---
+++ Init.js
@@ -0,0 +1,20 @@
+/**
+ * Init- Git init is used to create a new repo
+ * Create a new folder
+ * go to that folder
+ * Enter command in terminal >ls
+ * >ls -h (which will tell wheather git is intialised or not)
+ * >git init
+ * >now check again >ls -h (now some .git file you will find but it will be hidden)
+ * in terminal tou can see.
+ * 
+ * Go to git hub 
+ * Create a new repo without README.me file
+ * Then you can get the new repo https link copy that one
+ * Come to VS code
+ * >git remote add origin https......link (which build a connection between your local repo and remote repo
+ * Nothing but git hub new repo)
+ * 
+ * Yo check the status of wheather your local repo is connected to remote repo
+ * >git remote -v
+ */
---
+++ README.md
@@ -0,0 +1,3 @@
+# Init Project
+<br>
+Author:venu
